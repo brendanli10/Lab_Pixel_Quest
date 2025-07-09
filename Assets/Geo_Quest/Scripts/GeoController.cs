abb0adcd4f7ec26ff6c70e27ba88dfe871367563
brendanli10 <brendanchip4@gmail.com>
2025-07-09 12:06:55 -0400

    2dPhysics Continued and intro to C#

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+    string variable1 = "Hello";
+    int var1 = 3;
+    // Start is called before the first frame update
+    //We combined var1 and var2
+    void Start()
+    {
+        string variable2 = " Hi how are you today" +
+            "";
+        Debug.Log("Hello World");
+        Debug.Log(variable1 + variable2);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(var1++);
+
+        
+    }
+}
