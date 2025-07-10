85dbfa6947570d3cf990e9e8ceeb4a1ed48e8c8f
brendanli10 <brendanchip4@gmail.com>
2025-07-10 12:06:34 -0400

    7/10 AM Session

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -4,23 +4,54 @@ using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
+    //global vars
     string variable1 = "Hello";
     int var1 = 3;
+    private Rigidbody2D rb;
+    int var2 = 3;
+    public int speed = 8;
+
     // Start is called before the first frame update
-    //We combined var1 and var2
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         string variable2 = " Hi how are you today" +
             "";
         Debug.Log("Hello World");
         Debug.Log(variable1 + variable2);
+
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(var1++);
 
-        
+        float xInput = Input.GetAxis("Horizontal");
+        Debug.Log(xInput);
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+
+        /*
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+
+        //Debug.Log(var1++);
+        //moves the square to the right
+        //transform.position += new Vector3(0.005f, 0, 0); */
+
     }
 }
+
