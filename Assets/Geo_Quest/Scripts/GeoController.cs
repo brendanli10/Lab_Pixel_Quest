a6286a89431334d9ce19631dad07c4ada6f27d05
brendanli10 <brendanchip4@gmail.com>
2025-07-15 16:29:40 -0400

    7/15 Session (Game GeoQuest)

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -9,19 +9,18 @@ public class GeoController : MonoBehaviour
  //   string variable1 = "Hello";
    // int var1 = 3;
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
    // int var2 = 3;
-    public int speed = 8;
+    public int speed = 4;
     public string nextLevel = "Scene 2 GQ";
+    public string thirdLevel = "Scene 3 GQ";
+    public string EndPage = "Scene 4 GQ";
+    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-       /* string variable2 = " Hi how are you today" +
-            "";
-        Debug.Log("Hello World");
-        Debug.Log(variable1 + variable2);
-        Debug.Log(variable1 + variable2); */
-
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -29,10 +28,59 @@ public class GeoController : MonoBehaviour
     {
 
         float xInput = Input.GetAxis("Horizontal");
-        //Debug.Log(xInput);
         rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
         
-        /* Key Controls for Movement
+          if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        {
+            sr.color = new Color (1,0,0); //red
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) 
+        {
+            sr.color = new Color (0,1,0); //green
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) 
+        {
+            sr.color = new Color(0,0,1); //blue
+        }
+       
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        switch (collision.tag)
+        {
+            case "Death":
+                {
+                    string thisLevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thisLevel);
+                    break;
+                }
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+                }
+            case "LVL2FINISH":
+                {
+                    SceneManager.LoadScene(thirdLevel);
+                    break;
+                }
+            case "The End":
+                {
+                    SceneManager.LoadScene(EndPage);
+                    break;
+                }
+            }
+        }
+        
+    }
+
+
+
+
+
+   /* Key Controls for Movement
         if (Input.GetKeyDown(KeyCode.W))
         {
             transform.position += new Vector3(0, 1, 0);
@@ -48,32 +96,4 @@ public class GeoController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             transform.position += new Vector3(-1, 0, 0);
-        }
-
-        //Debug.Log(var1++);
-        //moves the square to the right
-        //transform.position += new Vector3(0.005f, 0, 0); */
-
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //  Debug.Log(collision.tag);
-
-        switch (collision.tag)
-        {
-            case "Death":
-                {
-                    string thisLevel = SceneManager.GetActiveScene().name;
-                    SceneManager.LoadScene(thisLevel);
-                    break;
-                    }
-            case "Finish":
-                {
-                    SceneManager.LoadScene(nextLevel);
-                    break;
-            }
-        }
-        
-    }
-}
-
+        } */
