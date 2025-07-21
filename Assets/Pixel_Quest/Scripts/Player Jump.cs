0983039e1a7882823dad20107052d94de95e2e92
brendanli10 <brendanchip4@gmail.com>
2025-07-21 19:49:42 -0400

    7/21 (Player Jump)

--- Assets/Pixel_Quest/Scripts/Player Jump.cs
+++ Assets/Pixel_Quest/Scripts/Player Jump.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb; 
+    public float JumpForce= 10f;
+    public float CapsuleHeight= 0.25f;
+    public float CapsuleRadius= 0.08f;
+
+    public Transform feetCollider;
+    public LayerMask groundMask;
+    private bool _groundCheck;
+    private float fallForce = 3;
+    private Vector2 gravityForce;
+    public bool _waterCheck;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        gravityForce= new Vector2 (0f, Physics2D.gravity.y);
     }
 
-    // Update is called once per frame
+ private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag=="Water")
+        {
+            _waterCheck= true;
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+       if (other.tag=="Water"){
+        _waterCheck= false;
+       }
+    
+}
+
+
     void Update()
     {
+         _groundCheck = Physics2D.OverlapCapsule(feetCollider.position, new Vector2
+        (CapsuleHeight,CapsuleRadius), CapsuleDirection2D.Horizontal,0,groundMask);
+    
+         if (Input.GetKeyDown(KeyCode.Space)&& (_groundCheck || _waterCheck))
+        {
+           rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+        }
         
+        if (rb.velocity.y<0)
+        {
+            rb.velocity += gravityForce * (fallForce*Time.deltaTime);
+        }
     }
+
 }
