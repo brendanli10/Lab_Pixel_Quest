18c1d95b387d887353d0ae63cdbd15179c7ef1ef
brendanli10 <brendanchip4@gmail.com>
2025-07-26 18:52:13 -0400

    HOHW3

---
+++ Assets/Hands-On Homework #3/Scripts/NextLevel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NextLevel : MonoBehaviour
+{
+    public string nextLevel = "Level2";
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        switch (collision.tag)
+        {
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+                }
+        }
+    }
+}
